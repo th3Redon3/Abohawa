b49780fcd29f6338a2208ef7a20dcf1fbbe7cb73
TheRedOne <146045112+th3Redon3@users.noreply.github.com>
2024-09-02 20:04:40 -0400

    Small Bugs
    JSON parse bugs; fixed by Felix
    
    Fix it Felix!!

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -2,20 +2,16 @@ import javax.swing.*;
 
 public class AppLauncher {
     public static void main(String[] args) {
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                // delay our weather app GUI
+                new AbohawaGUI().setVisible(true);
 
-        var checker = new ImageChecker();
+            }
 
-        checker.loadImage("src/assets/windspeed.png");
-
-//        SwingUtilities.invokeLater(new Runnable() {
-//            @Override
-//            public void run() {
-//                // delay our weather app GUI
-//                new AbohawaGUI().setVisible(true);
-//
-//            }
-//
-//        });
+        });
 
     }
 }
